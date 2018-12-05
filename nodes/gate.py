b83e1badcb2bc9053af2d4c8ac7dffc1811be08d
gknipe <knipegp@gmail.com>
2018-12-05 09:55:26 -0500

    Moved input summing from the Gate to a new node.

--- nodes/gate.py
+++ nodes/gate.py
@@ -20,15 +20,8 @@ class Gate(node.Node):
                           + self.time_step*(self.a_rate*(self.b_max_level-self.output[idx-1])
                                             - (self.s_input[idx-1]*self.output[idx-1]))
 
-    def execute(self, i_input, j_input=list()):
-        # Determine the S_input to the gate depending on whether a phasic input is being applied to the gate.
-        if not j_input:
-            assert len(i_input) == self.steps, "Length of input and output lists must be consistent."
-            self.s_input = i_input
-        else:
-            assert len(i_input) == len(j_input) == self.steps, "Length of input and output lists must be consistent."
-
-            # Element-wise addition of the input lists.
-            self.s_input = list(map(operator.add, i_input, j_input))
+    def execute(self, s_input):
+        assert len(s_input) == self.steps, "Length of input and output lists must be consistent."
+        self.s_input = s_input
 
         self.calculate_output()
---
+++ nodes/input_sum.py
@@ -0,0 +1,26 @@
+import node
+import operator
+
+
+class InputSum(node.Node):
+
+    def __init__(self, time_step=.001, steps=1000):
+        super(InputSum, self).__init__(time_step, steps)
+
+        self.i_input = self.j_input = [0] * steps
+
+    def calculate_output(self):
+        # Element-wise addition of the input lists.
+        self.output = list(map(operator.add, self.i_input, self.j_input))
+
+    def execute(self, i_input, j_input=list()):
+        # Determine the S_input to the gate depending on whether a phasic input is being applied to the gate.
+        if not j_input:
+            assert len(i_input) == self.steps, "Length of input and output lists must be consistent."
+            self.i_input = i_input
+        else:
+            assert len(i_input) == len(j_input) == self.steps, "Length of input and output lists must be consistent."
+            self.i_input = i_input
+            self.j_input = j_input
+
+        self.calculate_output()
