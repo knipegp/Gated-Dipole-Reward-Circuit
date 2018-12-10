710758ea2f9603b870746d0dc3242239fedad61e
gknipe <knipegp@gmail.com>
2018-12-10 18:14:00 -0500

    Added test for average number of iterations to addiction.

--- run_sim.py
+++ run_sim.py
@@ -1,5 +1,6 @@
 import normal_gated_dipole
 import modulated_circuit
+from matplotlib import pyplot as plt
 
 
 def main():
@@ -35,6 +36,38 @@ def main():
                                    len(modulated_circ.phasic_input.output)-1])
 
     modulated_circ.plot_uses()
+
+    list_of_runs = list()
+    for idx in range(1000):
+        runs_to_addiction = 0
+        addiction_average = modulated_circuit.ModulatedCircuit(a_rate=5,
+                                                               b_max_level=1,
+                                                               b_reduction_rate=.03,
+                                                               time_step=.001,
+                                                               steps=1000,
+                                                               tonic_max=1,
+                                                               tonic_min=0,
+                                                               phasic_duty_cycle=.5,
+                                                               phasic_max=10,
+                                                               phasic_min=.5,
+                                                               min_reduction=1 / 5)
+
+        while addiction_average.phasic_input.phasic_probability < .99:
+            addiction_average.execute(runs=1)
+            runs_to_addiction = runs_to_addiction + 1
+
+        list_of_runs.append(runs_to_addiction)
+
+        # Used to check progress. Make sure machine isn't frozen.
+        if idx % 50 == 0:
+            print(idx)
+
+    plt.Figure()
+    plt.hist(list_of_runs, bins=10)
+    plt.title('Number of Runs to Addiction Histogram')
+    plt.ylabel('Iterations of Model')
+    plt.xlabel('Number of Runs to Addiction')
+    plt.show()
 
 
 if __name__ == '__main__':
